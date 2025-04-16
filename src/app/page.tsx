a88225365af8531414d14514d906fd03f7439a2a
pratayay99 <pratyay.chakraborty@thefirstpenny.tech>
2025-04-16 15:57:38 +0530

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Hello world change9!</h1>
+      <h1 className="text-3xl font-bold underline">Hello world change8!</h1>
     </div>
   );
 }
+// fly launch--config fly.toml--name fp - app - 1 --yes
