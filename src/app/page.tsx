f1fb3efa0ad56a35dfa90e25b98330059f290140
Pratyay Chakraborty <pratyay.chakraborty@thefirstpenny.tech>
2025-04-17 12:09:15 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Hello world change30!</h1>
+      <h1 className="text-3xl font-bold underline">Hello world change500!</h1>
     </div>
   );
 }
