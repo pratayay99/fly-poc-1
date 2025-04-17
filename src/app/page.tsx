35041eaff2f084d1b0ee56b7249f9439928782f2
Pratyay Chakraborty <pratyay.chakraborty@thefirstpenny.tech>
2025-04-17 17:51:10 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Hello world changed </h1>
+      <h1 className="text-3xl font-bold underline">Hello world changed holo </h1>
       <p>Env:{process.env.REACT_APP_APP_ENV}</p>
     </div>
   );
