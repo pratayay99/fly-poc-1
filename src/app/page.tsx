993287539b574b3cc345cf5878b3217a17ead971
pratayay99 <pratyay.chakraborty@thefirstpenny.tech>
2025-04-17 14:15:21 +0530

    add env

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,8 @@ export default function Home() {
   return (
     <div>
       <h1 className="text-3xl font-bold underline">Hello world change500!</h1>
+      <p>{process.env.APP_ENV}</p>
     </div>
   );
 }
-// fly launch --config fly.toml--name fp-app-1 --yes
+// fly launch --config fly.toml --name fp-app-1 --yes
