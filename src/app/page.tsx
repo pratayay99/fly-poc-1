4e3fe7bf79da1d1a3b368645b48c195a1016ff66
pratayay99 <pratyay.chakraborty@thefirstpenny.tech>
2025-04-17 14:17:10 +0530

    add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Hello world change500!</h1>
-      <p>{process.env.APP_ENV}</p>
+      <h1 className="text-3xl font-bold underline">Hello world changed!</h1>
+      <p>Env:{process.env.APP_ENV}</p>
     </div>
   );
 }
