aa5a931fb6f8642de83fc871c7b9668010fa4359
Pratyay Chakraborty <pratyay.chakraborty@thefirstpenny.tech>
2025-04-21 17:30:45 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
     <div>
       <h1 className="text-3xl font-bold underline">Hello world changed</h1>
       <p>Env:{process.env.REACT_APP_APP_ENV}</p>
-      <p>update new pratyay</p>
+      <p>update new pratyay 5</p>
     </div>
   );
 }
