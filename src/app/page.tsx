19bac17842808c39f687c4921f3a42ae0b3a1ef8
pratayay99 <pratyay.chakraborty@thefirstpenny.tech>
2025-04-21 12:21:05 +0530

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
     <div>
       <h1 className="text-3xl font-bold underline">Hello world changed holo </h1>
       <p>Env:{process.env.REACT_APP_APP_ENV}</p>
-      <p>update</p>
+      <p>update 6</p>
     </div>
   );
 }
