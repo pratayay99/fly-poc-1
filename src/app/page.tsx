90f03c941d4b7feca6153ee3a85361748c0e6e61
pratayay99 <pratyay.chakraborty@thefirstpenny.tech>
2025-04-24 16:58:46 +0530

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ export default function Home() {
       <h1 className="text-3xl font-bold underline">Hello world Updated 🤗😊✔️</h1>
       <p>Env:{process.env.VITE_REACT_APP_APP_ENV}</p>
       <p>update new pratyay new 8wwwv new 😂😂</p>
+      <img src="https://plus.unsplash.com/premium_photo-1664474619075-644dd191935f?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1hZ2V8ZW58MHx8MHx8fDA%3D" alt="" />
     </div>
   );
 }
